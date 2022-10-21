66cea1cc7db1d7ef72fa0243e7fa84926212c4bd
Pedro Contreras Avendano <pedro.contreras.avendano@gmail.com>
2022-10-20 21:33:35 -0400

    Ãc10 - Ajout du css pour l<extension du carrousel

--- html/wp-content/plugins/pca_carrousel/js/carrousel.js
+++ html/wp-content/plugins/pca_carrousel/js/carrousel.js
@@ -1,3 +1,14 @@
 (function () {
     console.log("carrousel");
+    let elmBtnModale = document.querySelector(".btn_modale");
+    let elmBtnModaleFermer = document.querySelector(".btn_fermer");
+    let elmCarrousel = document.querySelector(".carrousel");
+    elmBtnModale.addEventListener("mousedown" , function () {
+    console.log("bouton boîte modale");
+    elmCarrousel.classList.add("carrousel-ouvrir");
+    });
+    elmBtnModaleFermer.addEventListener("mousedown" , function () {
+        console.log("bouton boîte modale");
+        elmCarrousel.classList.remove("carrousel-ouvrir");
+    });
 })();
--- html/wp-content/plugins/pca_carrousel/pca_carrousel.php
+++ html/wp-content/plugins/pca_carrousel/pca_carrousel.php
@@ -8,34 +8,31 @@
 * version 0.1
 */
 
-/*
-filemtime()  // retourne en milliseconde le temps de la dernière sauvegarde
-plugin_dir_path() // retourne le chemin du répertoire du plugin
-__FILE__ // le fichier en train de s'exécuter
-wp_enqueue_style() // Intègre le link:css dans la page
-wp_enqueue_script() // intègre le script dans la page
-wp_enqueue_scripts // le hook
-*/
+
+
+function genere_boite(){
+	$contenu = "
+    <button class='btn_modale'>boîte modale</button>
+    <div class='carrousel'>
+        <button class='btn_fermer'>X</button>
+    </div>";
+	return $contenu;
+}
+add_shortcode('pca_carrousel', 'genere_boite');
 
 function pcac_enqueue() {
 	$version_css = filemtime(plugin_dir_path(__FILE__)."style.css");
 	$version_js = filemtime(plugin_dir_path(__FILE__)."js/carrousel.js");
 	wp_enqueue_style("pca_style_carrousel",
-	plugin_dir_url(),"style.css",
-	array(),
+		plugin_dir_url(__FILE__)."sass/style.css",
+		array(),
 		$version_css,
 		false);
 
-	wp_enqueue_js("pca_js_carrousel",
-		plugin_dir_url(),"js/carrousel.js",
+	wp_enqueue_script("pca_js_carrousel",
+		plugin_dir_url(__FILE__) . "js/carrousel.js",
 		array(),
 		$version_js,
-		false);
+		true);
 }
 add_action("wp_enqueue_scripts","pcac_enqueue");
-
-function genere_boite(){
-	$contenu = "<div class='carrousel'>Carrousel</div>";
-	return $contenu;
-}
-add_shortcode('pca_carrousel', 'genere_boite');
